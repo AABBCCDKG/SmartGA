243711aadbd7c9407032ab3916b879ad3240d58f
dongwang <156266097+AABBCCDKG@users.noreply.github.com>
2024-08-08 15:09:54 -0400

    Update geneticalgo.py

--- geneticalgo.py
+++ geneticalgo.py
@@ -11,13 +11,6 @@ class GeneticAlgorithm:
         self.population_size = population_size
         self.generations = generations
         self.population = []
-    
-    
-    #def crossover(self, parent1, parent2):
-        #cut = random.randint(1, min(len(parent1.instructions), len(parent2.instructions)) - 1) # 修改交叉点的选择
-        #child1_instructions = parent1.instructions[:cut] + parent2.instructions[cut:]
-        #child2_instructions = parent2.instructions[:cut] + parent1.instructions[cut:]
-        #return Function(child1_instructions, ""), Function(child2_instructions, "")
 
     def extract_index(self,input_string):
         """
@@ -33,7 +26,7 @@ class GeneticAlgorithm:
         else:
             return None
         
-    def mutate(self, function): # call_chatgpt, 输入方程对象，返回修改指令，
+    def mutate(self, function): # call_chatgpt, 
         print("\nMutation:\n")
         order = "function." + llm_for_mutation(str(function.track))
         order = order.replace('x', 'y')
@@ -65,39 +58,26 @@ class GeneticAlgorithm:
         function.update_expression()
         print(function.instructions)
         print(function.track)
-        #function.update_expression()
 
     def run(self):
-        # 生成初始种群
         population = [llm_for_initial_generation(str(self.desired_output)) for _ in range(self.population_size)]
         for instruction in population:
-            #print("instruction: ")
-            #print(instruction)
             func = Function.create_from_instructions(instruction)
             self.population.append(func)
-        #print(f"Initial population for a single function: {[func.expression for func in population]}")
         for generation in range(self.generations):
             print(f'\nGeneration {generation}')
             scores = []
             for func in self.population:
-                #print(f"\nEvaluating function: {func.expression}")
                 result = func.calculate(self.time_value)
-                #print(f"Calculation result: {result}")
-                #print(f"Desired output: {self.desired_output}")
-            
-                score = func.evaluate_similarity(result, self.desired_output)
-                #print(f"Similarity score: {score}")               
+                score = func.evaluate_similarity(result, self.desired_output)     
                 scores.append((score, func))
                 if generation == 0:
                     func.add_track(str(score['similarity_score']), result, func.instructions, func.expression, self.desired_output)
-        #print("\nScores: ", [(score, func.expression) for score, func in scores])
         
-            # 按 'similarity_score' 排序
             print("\nScores: ", [(score, func.expression) for score, func in scores])
             scores.sort(reverse=True, key=lambda x: x[0]["similarity_score"])
-            best_functions = [func for _, func in scores[:self.population_size // 2]] #取前一半的函数
+            best_functions = [func for _, func in scores[:self.population_size // 2]] 
             print(f"\nBest functions: {[func.expression for func in best_functions]}\n")
-        # 生成新的种群
             temp_population = best_functions.copy()
             new_population = best_functions.copy()
             for new_func in temp_population:
@@ -107,13 +87,7 @@ class GeneticAlgorithm:
                 self.mutate(temp_func)
                 new_population.append(temp_func)
             self.population = new_population
-            #print(f"New population: {[func.expression for func in population]}")
-    
-        # 找到最佳函数
         best_function = max(self.population, key=lambda func:
             func.evaluate_similarity(func.calculate(self.time_value), self.desired_output)['similarity_score'])
-        
-        #print(f"\nBest function found: {best_function.expression}")
-        #print(f"Best function score: {best_function.evaluate_similarity(best_function.calculate(self.time_value), self.desired_output)["similarity_score"]}")
         return best_function
     
